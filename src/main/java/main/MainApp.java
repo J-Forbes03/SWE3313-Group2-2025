0db06e8d1b6c601b81964159ed4a7a7e21854ee4
J-Forbes03 <forbesjames03@gmail.com>
2025-04-28 19:36:23 -0400

    Documented and finished

--- src/main/java/main/MainApp.java
+++ src/main/java/main/MainApp.java
@@ -5,11 +5,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
+/**
+ * Main entry point for the Restaurant Automation application.
+ * Handles launching the application and switching between scenes.
+ */
 public class MainApp extends Application {
 
+    /**
+     * Primary stage used for displaying scenes.
+     */
     private static Stage primaryStage;
 
+    /**
+     * Starts the application by loading the login scene.
+     *
+     * @param stage The primary stage provided by the JavaFX runtime.
+     * @throws Exception if the FXML file cannot be loaded.
+     */
     @Override
     public void start(Stage stage) throws Exception {
         primaryStage = stage;
@@ -21,7 +33,12 @@ public class MainApp extends Application {
         primaryStage.show();
     }
 
-
+    /**
+     * Changes the current scene to a new one based on the provided FXML file.
+     *
+     * @param fxml The name of the FXML file to load (e.g., "FloorLayout.fxml").
+     * @throws Exception if the FXML file cannot be loaded.
+     */
     public static void changeScene(String fxml) throws Exception {
         FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/view/" + fxml));
         Scene scene = new Scene(loader.load());
@@ -29,6 +46,11 @@ public class MainApp extends Application {
         primaryStage.centerOnScreen();
     }
 
+    /**
+     * Main method to launch the application.
+     *
+     * @param args Command-line arguments (not used).
+     */
     public static void main(String[] args) {
         launch(args);
     }
